d6f6b7c0d90ad07191b31fac73f57253b5bf331f
artumarinn <artumarin.09@gmail.com>
2024-07-09 20:40:30 -0300

    codificacion REGISTER

--- src/model/methods.java
+++ src/model/methods.java
@@ -3,6 +3,7 @@ package model;
 
 public class methods {
     
+    // terminar metodo para agregar usuario
     public static boolean InsertarUsuario(user x) {
         
         Connection cx=conexion.Conectar();
